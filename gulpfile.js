eaa62349352073c7a3c8522e525b02c849694524
Roman <kekisreal228@gmail.com>
2021-12-25 10:19:10 +0200

    idk commit (correct setting before start)

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,12 @@
 const gulp = require('gulp');
 const browserSync = require('browser-sync');
 const sass = require('gulp-sass')(require('sass'));
-
+const rename = require("gulp-rename");
+const cleanCSS = require('gulp-clean-css');
+const autoprefixer = require('gulp-autoprefixer');
 
 // Static server
-gulp.task('server', function() {
+gulp.task('server', function() { //работает по типу live-server
     browserSync.init({
         server: {
             baseDir: "src"
@@ -12,11 +14,19 @@ gulp.task('server', function() {
     });
 });
 
-gulp.task('styles', function(){
+gulp.task('styles', function(){ //работает когда файл компилиться
     return gulp.src("src/sass/*.+(scss|sass)") //возможно тут будет ошибка из-за того, что не указал папку base.
-     .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-     .pipe(gulp.dest("src/css")) //положить скомпилированный файл в определённый адресс
-     .pipe(browserSync.stream()); //перезапустить браузер    
+    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError)) //style.sass -> style.css
+    .pipe(rename({
+        prefix: "",
+        suffix: ".min" //style.css -> style.min.css
+    }))
+    .pipe(autoprefixer({ //работа autoprefixer
+        cascade: false
+    }))
+    .pipe(cleanCSS({compatibility: 'ie8'})) // работа cleanCss (css-файл после всего будет очищаться)
+    .pipe(gulp.dest("src/css")) //положить скомпилированный файл в определённый адресс
+    .pipe(browserSync.stream()); //перезапустить браузер    
 });
 
 
